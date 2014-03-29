2a1478787a2b33651add14168b08a0027b827b6d
fermatliu <fermatliu@gmail.com>
2014-03-30 07:46:01 +0800

    Add Lenovo A850 Source Code

---
+++ Source/sys/noautodep.mk
@@ -0,0 +1,11 @@
+#pragma noautodep */.git/*
+$(local-intermediates-dir)/libbcc-stamp.c :
+
+#pragma noautodep */out/target/product/generic/system/bin/cat
+$(linked_module) :
+
+#pragma nolookup noproguard.classes-with-local.dex
+#pragma nolookup noproguard.classes.dex
+#pragma nolookup javalib.jar
+#pragma nolookup classes-full-debug.jar
+out/target/common/docs/doc-comment-check-timestamp :
